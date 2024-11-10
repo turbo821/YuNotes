738cbbbb8df603c5ed279c324c5a74f9f13b6f70
turbo821 <turbo3735@gmail.com>
2024-11-11 00:54:39 +0300

    add valid

--- YuNotes/Controllers/AuthController.cs
+++ YuNotes/Controllers/AuthController.cs
@@ -1,19 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using YuNotes.ViewModels;
 
 namespace YuNotes.Controllers
 {
     public class AuthController : Controller
     {
+        [HttpGet]
         [Route("/login")]
         public IActionResult Login()
         {
             return View();
         }
 
+        [HttpPost]
+        [Route("/login")]
+        public async Task<IActionResult> LoginAsync(LogInViewModel request)
+        {
+            Console.WriteLine($"{request.Email} -- {request.Password}");
+            return RedirectToAction("Catalog", "Notes");
+        }
+
+        [HttpGet]
         [Route("/signup")]
         public IActionResult SignUp()
         {
             return View();
+        }
+
+        [HttpPost]
+        [Route("/signup")]
+        public async Task<IActionResult> SignUpAsync(SignInViewModel model)
+        {
+            Console.WriteLine("Hello");
+            return RedirectToAction("Login");
         }
 
         [Route("/inputemail")]
---
+++ YuNotes/ViewModels/SignInViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security;
+
+namespace YuNotes.ViewModels
+{
+    public class SignInViewModel
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Длина от 3 до 30 символов")]
+        public string? Nickname { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string? PasswordConfirm { get; set; }
+    }
+}
